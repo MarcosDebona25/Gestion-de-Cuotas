8581f3dc706c0b761837fc7686f266304c90e91a
Marcos Debona <marcosjd2005@gmail.com>
2025-04-27 00:54:13 -0300

    Subiendo el proyecto crudo

---
+++ gestion-cutoas/src/main/java/com/marcos/gestioncuotas/GestionCuotasApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.gestioncuotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCuotasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCuotasApplication.class, args);
+    }
+
+}
